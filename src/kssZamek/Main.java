b5ced13f8c8a2f26767e2a27ccd559a75eaa7607
Karol Piasnik <piasnik.karol@outlook.com>
2019-04-06 21:14:30 +0200

    refactor

--- src/kssZamek/Main.java
+++ src/kssZamek/Main.java
@@ -1,69 +1,67 @@
 package kssZamek;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
-        import java.io.File;
-        import java.io.FileNotFoundException;
 
 
 public class Main {
 
-    public static void rob(Lock zamek) throws InterruptedException {
+    public static void rob(Lock lock) throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
-        String proba;
-        int test=0;
-        while(test==0) {
+        String testing;
+        int test = 0;
+        while (test == 0) {
             System.out.println("Podaj kod do odblokowania zamka. Wpisz 0 aby wrocic do menu.");
-            proba=scanner.nextLine();
-            if(proba.equals("0"))
+            testing = scanner.nextLine();
+            if (testing.equals("0"))
                 return;
             else
-                test = zamek.sprawdz(proba);
+                test = lock.check(testing);
         }
-        if(test==2) {
+        if (test == 2) {
             System.out.println("Elo admin. Co teraz?");
         }
     }
 
-    public static void main(String[] args)  throws InterruptedException, FileNotFoundException  {
+    public static void main(String[] args) throws InterruptedException, FileNotFoundException {
 
-        File plik = new File("haslo.txt");
-        Scanner in=new Scanner(plik);
-        String haslo, hasloA;
-        haslo=in.nextLine();
-        hasloA=in.nextLine();
+        Path passwordFile = Paths.get("src/kssZamek/haslo.txt");
+        Scanner in = new Scanner(passwordFile.toFile());
+        String password, passwordA;
+        password = in.nextLine();
+        passwordA = in.nextLine();
 
-        Lock zamek = new Lock(haslo, hasloA);
+        Lock lock = new Lock(password, passwordA);
 
         Scanner scan = new Scanner(System.in);
-        String proba;
+        String testing;
         int test1;
 
-        while(true) {
+        while (true) {
             System.out.println("--- MENU ---");
             System.out.println("Wybierz czynność:");
             System.out.println("1- Odblokuj kssZamek.Lock");
             System.out.println("2- Zmien hasło");
             System.out.println("0- Zakończ program");
-            proba=scan.nextLine();
+            testing = scan.nextLine();
 
-            if(proba.equals("0")) {
+            if (testing.equals("0")) {
                 return;
-            }
-            else if(proba.equals("1")) {
-                rob(zamek);
+            } else if (testing.equals("1")) {
+                rob(lock);
                 continue;
-            }
-            else if(proba.equals("2")) {
+            } else if (testing.equals("2")) {
                 System.out.println("Podaj kod administratora. Wpisz '0' jeśli chcesz wrócić do głównego menu");
-                proba=scan.nextLine();
-                if(proba.equals("0")) {
+                testing = scan.nextLine();
+                if (testing.equals("0")) {
                     continue;
-                }
-                else
-                    zamek.admin(proba);
+                } else
+                    lock.admin(testing);
 
-            }
-            else{
+            } else {
                 System.out.println("Wpisano niepoprawną wartość.");
                 System.out.println("");
                 continue;
@@ -73,7 +71,6 @@ public class Main {
 
 
 }
-
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
