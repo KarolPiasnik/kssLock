55a770c0e5488e6462818ccadf3f3b29b93366a6
Karol Piasnik <piasnik.karol@outlook.com>
2019-05-18 09:55:16 +0200

    lock basic version

--- src/kssZamek/Lock.java
+++ src/kssZamek/Lock.java
@@ -1,126 +1,68 @@
 package kssZamek;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.Scanner;
-
 public class Lock {
-    private String code;
-    private String adminCode;
-    private int counter = 1;
 
+    private String password;
 
-    public Lock(String code, String adminCode) {
-        this.code = code;
-        this.adminCode = adminCode;
+    private String adminPassword;
+
+    private Boolean locked;
+
+    private Boolean adminBlocked = false;
+
+    private  Boolean adminMode = false;
+
+    private Integer failedAttempts = 0;
+
+    public Lock(String password, String adminPassword) {
+        this.password = password;
+        this.adminPassword = adminPassword;
+        locked = Boolean.FALSE;
     }
 
-    public int check(String kodTest) throws InterruptedException {
-        Scanner scan = new Scanner(System.in);
-        if (counter < 3) {
-            if (kodTest.equals(this.code)) {
-                return unlock();
-
-            } else {
-                System.out.println("Zły kod, spróbuj ponownie");
-                counter++;
-                return 0;
-            }
-        } else if (counter == 3) {
-            if (kodTest.equals(this.code)) {
-                return unlock();
-            } else {
-                System.out.println("3 niepoprawne próby, zamek zablokowany na 5s. Proszę czekać...");
-                Thread.sleep(5000);
-                counter++;
-                return 0;
-            }
-        } else if (counter == 4) {
-            if (kodTest.equals(this.code)) {
-                return unlock();
-
-            } else {
-                System.out.println("4 niepoprawne próby, zamek zablokowany na 10s. Proszę czekać...");
-                Thread.sleep(10000);
-                System.out.println("UWAGA! W przypadku kolejnego niepowodzenia będzie wymagana interwencja administratora!");
-                counter++;
-                return 0;
-            }
-        } else {
-            if (kodTest.equals(this.code)) {
-                return unlock();
-
-            } else {
-                System.out.println("5 niepoprawnych prób, zamek zablokowany. Poproś o pomoc administratora.");
-                return 2;
-            }
+    public Boolean unlock(String inputPassword) throws AlreadyUnlockedException, AdminBlockedException {
+        if(adminBlocked){
+            throw new AdminBlockedException();
         }
-
-
-    }
-
-    public void admin(String kod) throws FileNotFoundException {
-        if (kod.equals(adminCode)) {
-            System.out.println("Przyznano dostęp administratora.");
-            System.out.println("Podaj stary kod do odblokowania zamka:");
-            Scanner scan = new Scanner(System.in);
-            String test = scan.nextLine();
-
-            if (test.equals(getCode())) {
-
-                PrintWriter writer = new PrintWriter("src/kssZamek/haslo.txt");
-
-                System.out.println("Podaj nowy kod:");
-                test = scan.nextLine();
-                setCode(test);
-                System.out.println("Nowy kod to: " + test);
-                writer.println(this.code);
-                writer.println(this.adminCode);
-                writer.close();
-            } else {
-                System.out.println("Niepoprawny kod do odblokowania zamka.");
+        if (!Boolean.TRUE.equals(locked)) {
+            if (this.password.equals(inputPassword)) {
+                locked = Boolean.TRUE;
             }
-
-
-        } else
-            System.out.println("Zły kod administratora.");
-        System.out.println("");
-        return;
+            else {
+                if(failedAttempts >= 5){
+                    adminBlocked = Boolean.TRUE;
+                }
+                ++failedAttempts;
+            }
+            return locked;
+        }
+        else{
+            throw new AlreadyUnlockedException();
+        }
     }
 
-
-    private int unlock() {
-        counter = 1;
-        Scanner scan = new Scanner(System.in);
-        System.out.println("||| ZAMEK ODBLOKOWANY |||");
-        System.out.println("Wpisz 0 aby zablokować zamek.");
-        String test = "";
-        while (!test.equals("0"))
-            test = scan.nextLine();
-        System.out.println("||| ZAMEK ZABLOKOWANY |||");
-        System.out.println("");
-        return 1;
+    public void lock(){
+        locked = Boolean.TRUE;
     }
 
-
-    public String getCode() {
-        return code;
+    public Boolean enterAdminMode(String inputPassword) throws AlreadyInAdminModeException {
+        if (!Boolean.TRUE.equals(adminMode)) {
+            if (this.adminPassword.equals(inputPassword)) {
+                adminMode = Boolean.TRUE;
+            }
+            return adminMode;
+        }
+        else{
+            throw new AlreadyInAdminModeException();
+        }
     }
 
-
-    public void setCode(String code) {
-        this.code = code;
+    public void changePassword(String newPassword) throws NotInAdminModeException {
+        if(Boolean.TRUE.equals(adminMode)){
+            password = newPassword;
+            adminBlocked = Boolean.FALSE;
+        }
+        throw new NotInAdminModeException();
     }
-
-
-    public String getAdminCode() {
-        return adminCode;
-    }
-
-
-    public void setAdminCode(String adminCode) {
-        this.adminCode = adminCode;
-    }
-
 
 }
